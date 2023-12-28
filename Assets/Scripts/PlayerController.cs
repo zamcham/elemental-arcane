6c4f6a557c1460dbbf6439a4ae05665c245b66b5
Cristian Zambrano <criszamcham@gmail.com>
2023-12-28 09:54:20 -0700

    Start move point method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Transform movePoint;
     Vector2 moveInput;
     float moveSpeed = 5f;
+    LayerMask notWalkable;
 
     void Start()
     {
-        
+        movePoint.parent = null;
     }
 
     void Update()
@@ -27,8 +30,25 @@ public class PlayerController : MonoBehaviour
         Vector3 targetHorizontalPos = new Vector3(moveInput.x, 0f, 0f);
         Vector3 targetVerticalPos = new Vector3(0f, moveInput.y, 0f);
         
-
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(moveInput.x) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + targetHorizontalPos, 0.2f, notWalkable))
+                {
+                    movePoint.position += targetHorizontalPos;
+                }
+            }
+            else if (Mathf.Abs(moveInput.y) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + targetVerticalPos, 0.2f, notWalkable))
+                {
+                    movePoint.position += targetVerticalPos;
+                }
+            }
+        }
     }
 }
 
