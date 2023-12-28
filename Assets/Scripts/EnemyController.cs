6e2f1eb47044967603bd1e61b50f3b1a10e354e4
Cristian Zambrano <criszamcham@gmail.com>
2023-12-28 13:11:08 -0700

    Updated pathfinding method

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,28 +4,64 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    PlayerController player;
-    transform playerPosition;
+    public Transform player;
+    public Transform enemy;
+    public float moveSpeed = 1f; // Adjust the speed as needed
+    public List<Vector3> path;
+    private int currentWaypoint = 0;
 
     void Awake()
     {
-        player = FindObjectOfType<PlayerController>();
+        player = FindObjectOfType<PlayerController>().transform;
     }
 
     void Start()
     {
-        
+        CalculatePath();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void UpdateTargetPosition()
+    void CalculatePath()
     {
-        playerPosition = player.transform;
+        // Use A* algorithm to calculate the path
+        // You can implement your own A* algorithm or use a library like AstarPathfindingProject
+
+        // For this example, a simple straight line path is calculated
+        path = new List<Vector3>();
+        Vector3 currentPos = transform.position;
+
+        while (currentPos != player.position)
+        {
+            if (currentPos.x > player.position.x)
+            {
+                currentPos.x--;
+            }
+            else if (currentPos.x < player.position.x)
+            {
+                currentPos.x++;
+            }
+            else if (currentPos.y > player.position.y)
+            {
+                currentPos.y--;
+            }
+            else if (currentPos.y < player.position.y)
+            {
+                currentPos.y++;
+            }
+
+            path.Add(currentPos);
+        }
+
+        // Reverse the path to move from the enemy to the player
+        path.Reverse();
+
+        // Reset the waypoint index
+        currentWaypoint = 0;
     }
 
 }
